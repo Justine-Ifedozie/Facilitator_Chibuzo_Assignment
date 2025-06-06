7a49a337eb4908372075e80d4e69fbe523e7cf67
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-06 11:20:10 +0100

    LocalDate

---
+++ Practice/LearningDate.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+public class LearningDate {
+        public static void main (String[] args) {
+                LocalDate date = LocalDate.now();
+              System.out.println(date);
+
+
+
+
+
+
+        }
+}
